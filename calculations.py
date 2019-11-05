d9a3c68a584964622342b44bfda6c9b6a37496c7
Matt9172 <56167552+Matt9172@users.noreply.github.com>
2019-11-05 14:11:03 +0000

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,9 @@
+# a simple calculater that only adds up 2 numbers
+
+print("welcome, please enter a number")
+number_1 =  int(input ())
+print("please enter another number")
+number_2 = int (input ())
+print("The sum is", number_1 + number_2)
+print("The multiplication is", number_1 * number_2)
+print("The division is", number_1 / number_2)
---
+++ student_sweets.py
@@ -0,0 +1,7 @@
+
+print("welcome, please enter the number of sweets")
+number_of_sweets =  int(input ())
+print("please enter the number of students")
+number_of_students = int (input ())
+print("Each student gets", number_of_sweets // number_of_students, "sweets")
+print("The tutor gets", number_of_sweets % number_of_students, "sweets")
