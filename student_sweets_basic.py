eac98e45632e7ff16fb85ec6887b0265a26a5520
Matt9172 <56167552+Matt9172@users.noreply.github.com>
2019-11-14 23:09:20 +0000

    Add files via upload

---
+++ student_sweets_basic.py
@@ -0,0 +1,6 @@
+
+number_of_sweets = 19
+number_of_students = 22
+print("Each student gets", number_of_sweets // number_of_students, "sweets")
+print("The tutor gets", number_of_sweets % number_of_students, "sweets")
+
