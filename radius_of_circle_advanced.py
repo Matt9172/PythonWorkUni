6e445e162775fb9b7c43d8f35d992f45c4072ee7
Matt9172 <56167552+Matt9172@users.noreply.github.com>
2019-11-15 16:45:00 +0000

    Add files via upload

--- radius_of_circle_advanced.py
+++ radius_of_circle_advanced.py
@@ -1,5 +1,6 @@
-import math
+
 print("welcome, please enter the radius of the circle")
 radius = float(input ())
+from math import pi
 area = (pi) * (radius * radius)
 print("the area of the circle is",area)
