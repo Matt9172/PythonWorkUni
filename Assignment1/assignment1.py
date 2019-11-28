72b065e7614c2b2cf54752896ce2cb97148c86e9
Matthew McDougall <U1961826@unimail.hud.ac.uk>
2019-11-28 14:03:39 +0000

    From Linux

---
+++ Assignment1/assignment1.py
@@ -0,0 +1,12 @@
+__author__ = "Matthew McDougall"
+__email__ = "U1961826@unimail.hud.ac.uk"
+__licence__ = "The Unilicense"
+__version__ = "28/11/2019"
+
+print("Speed Camera")
+vehicle_list = []
+
+count = 0
+
+while count :1
+    reading = input("Input type of vehicle and speed e.g. H30 or C15" + str)
