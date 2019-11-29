0702fc061d16ce65c2ff0a372ebc111e0a922885
Matt9172 <bigmack4@hotmail.co.uk>
2019-11-29 14:22:45 +0000

    Up to date

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -3,10 +3,19 @@ __email__ = "U1961826@unimail.hud.ac.uk"
 __licence__ = "The Unilicense"
 __version__ = "28/11/2019"
 
-print("Speed Camera")
-vehicle_list = []
+print("Speed Camera Interface")
+vehicle_type = []
+vehicle_speed = []
+count = 1
 
-count = 0
+while count: 1
+reading = input("Input type of vehicle and speed e.g. H30 or C15" + str())
+if reading[0] in ("H", "L", "C",):
+    vehicle_type.append(reading[0])
+if reading[1:3]
 
-while count :1
-    reading = input("Input type of vehicle and speed e.g. H30 or C15" + str)
+
+    max = max(vehicle_speed)
+    min = min(vehicle_speed)
+    avg = sum(vehicle_speed) / len(vehicle_speed)
+    rounded = round(avg, 2)
