0a07fe7ab82542a431764c826818ff321020a807
Matt9172 <bigmack4@hotmail.co.uk>
2019-11-30 00:00:37 +0000

    Up to date

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -9,21 +9,22 @@ vehicle_speed = ()
 count = 1
 
 while 1:
-    reading = input("Input type of vehicle and speed e.g. H30 or C15").upper()
-    if reading in "END":
+    reading = input("Input type of vehicle and speed e.g. H30 or C15: ").upper()
+    if reading in "END".upper():
         break
     elif reading[0] in ("H", "L", "C",):
         vehicle_type.append(reading[0])
         print(vehicle_type)
-    vehicle_speed += (reading[1:3],)
-    print (vehicle_speed)
+        vehicle_speed += (reading[1:3],)
+        print(vehicle_speed)
+    else:
+        print("Please only enter data that starts with a H, L or C and has a two digit number afterwards e.g H30")
 
-#elif reading[1:3] in int:
-        #vehicle_speed.append(reading[1:3])
-       # print(vehicle_speed)
-
-
-   ## max = max(vehicle_speed)
-    #min = min(vehicle_speed)
-    #avg = sum(vehicle_speed) / len(vehicle_speed)
-    #rounded = round(avg, 2)
+no_of_vehicles = len(vehicle_type)
+highest_speed = max(vehicle_speed)
+lowest_speed = min(vehicle_speed)
+avg_speed = sum(vehicle_speed) / len(vehicle_speed)
+print("The number of vehicles was: ", no_of_vehicles)
+print("The highest speed was: ", highest_speed, "mph")
+print("The lowest speed was: ", lowest_speed, "mph")
+print("The average speed was: ", avg_speed, "mph")
