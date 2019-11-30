2b088648bcbbdcb0434ff42a19894e25a0168c1c
Matt9172 <bigmack4@hotmail.co.uk>
2019-11-30 17:13:08 +0000

    Assignment 1 finished

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -4,19 +4,21 @@ __licence__ = "The Unilicense"
 __version__ = "28/11/2019"
 
 print("Speed Camera Interface")
-vehicle_type = []
+vehicle_type = ()
 vehicle_speed = ()
 count = 1
 
 while 1:
-    reading = input("Input type of vehicle and speed e.g. H30 or C15: ").upper()
+    reading = str(input("Input type of vehicle and speed e.g. H30 or C15: ").upper())
+
+    if reading[0] in ("H", "L", "C",):
+        vehicle_type += (reading[0],)
+        print(vehicle_type)
+        reading_number = int(reading[1:])
+        vehicle_speed += (reading_number,)
+        print(vehicle_speed)
     if reading in "END".upper():
         break
-    elif reading[0] in ("H", "L", "C",):
-        vehicle_type.append(reading[0])
-        print(vehicle_type)
-        vehicle_speed += (reading[1:3],)
-        print(vehicle_speed)
     else:
         print("Please only enter data that starts with a H, L or C and has a two digit number afterwards e.g H30")
 
@@ -27,4 +29,4 @@ avg_speed = sum(vehicle_speed) / len(vehicle_speed)
 print("The number of vehicles was: ", no_of_vehicles)
 print("The highest speed was: ", highest_speed, "mph")
 print("The lowest speed was: ", lowest_speed, "mph")
-print("The average speed was: ", avg_speed, "mph")
+print("The average speed was: ", round(avg_speed, 1), "mph")
